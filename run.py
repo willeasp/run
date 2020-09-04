688174b5b56d2e0fe8cb71e52c053203c5c5ebe4
William Asp <wille.asp@live.se>
2020-09-04 23:57:51 +0200

    file I/O not working

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
-import sys, getopt, os
-
-# ,"hi:o:",["ifile=","ofile="]
+import sys, getopt, os.path, subprocess
 
 def help(full=False):
     if full:
@@ -15,32 +13,45 @@ def getFileNameType(file):
     fileName, fileType = os.path.splitext(file)
     return fileName, fileType
 
-def main(argv):
-    if len(argv) == 0:
-        print("Type the name of the file you want to compile and run.")
-        help()
+def addOptions(argv):
     command = ""
-    print(argv[1:])
     try:
         options, arguments = getopt.getopt(argv[1:], "ti:o:", "help")
     except getopt.GetoptError:
         print("failed to get opts")
         help()
         sys.exit(2)
-
-    """ for opt, arg in opts:
-        if opt == '--help':
-            print(help)
+    for opt, arg in options:
+        if opt in "--help":
+            help(full=True)
             sys.exit()
-        elif opt in ("-i", "--ifile"):
-            inputfile = arg
-        elif opt in ("-o", "--ofile"):
-            outputfile = arg
-        elif opt in ("-t", "--test"):
-            arguments.append(opt)
-    print("Arguments is: ", )
-    print('Input file is: ', inputfile)
-    print('Output file is: ', outputfile) """
+        if opt in ("-t", "--test"):
+            command += " -t "
+        if opt in ("-i", "--ifile"):
+            command += "< " + arg + " "
+        if opt in ("-o", "--ofile"):
+            command += "> " + arg + " "
+    for argument in arguments:
+        command += argument + " "
+    return command
+
+def compileFile (file):
+    fileName, fileType = getFileNameType(file)
+    compile_command = "gcc " + file + " -o " + fileName
+    print("Compiling: " + compile_command)
+    subprocess.run(compile_command.split())
+        
+def main(argv):
+    file = argv[0]
+    fileName, fileType = getFileNameType(file)
+    compileFile(file)
+    run_command =  "./" + fileName + addOptions(argv)
+    print("Running: " + run_command)
+    subprocess.run(run_command.split())
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    if len(sys.argv) == 1:
+        print("Type the name of the file you want to compile and run.")
+        help()
+        sys.exit(0)
+    main(sys.argv[1:])
