59eb39e53356f00e9b764361c5701811650c2934
William Asp <wille.asp@live.se>
2021-03-12 20:47:13 +0100

    added setup file

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+if [ $EUID -ne 0]
+then
+    echo "Please run as root to install to path."
+    exit
+fi
+
+chmod +x run.py
+ln -s /home/willster/dev/python/run/run.py /usr/local/bin/run
